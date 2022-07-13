31498971707a50e4a2cd08d63e52b0dd24351b17
Seungil <seungilbang@khu.ac.kr>
2022-07-13 12:19:22 +0900

    Add: super keyword Ex Code

---
+++ Java/OOP_Ex_Code/Inheritance_Encapsulation/Super.java
@@ -0,0 +1,25 @@
+// super 키워드에 대한 예제 코드
+
+package Inheritance_Encapsulation; // package declare for preventing error
+
+
+public class Super {
+    public static void main(String[] args) {
+        Lower l = new Lower();
+        l.callNum();
+    }
+}
+
+class Upper {
+    int count = 20; // super.count
+}
+
+class Lower extends Upper {
+    int count = 15; // this.count
+
+    void callNum() {
+        System.out.println("count = " + count);
+        System.out.println("this.count = " + this.count);
+        System.out.println("super.count = " + super.count);
+    }
+}
