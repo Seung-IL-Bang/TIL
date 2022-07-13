1b3f214abba848b054f482d475a822378950aecf
Seungil <seungilbang@khu.ac.kr>
2022-07-13 12:39:26 +0900

    Update: 추가 설명

--- Java/OOP_Ex_Code/Inheritance_Encapsulation/Super.java
+++ Java/OOP_Ex_Code/Inheritance_Encapsulation/Super.java
@@ -18,7 +18,7 @@ class Lower extends Upper {
     int count = 15; // this.count
 
     void callNum() {
-        System.out.println("count = " + count);
+        System.out.println("count = " + count); // super를 붙이지 않았기에 자신이 속한 클래스의 객체를 먼저 참조
         System.out.println("this.count = " + this.count);
         System.out.println("super.count = " + super.count);
     }
